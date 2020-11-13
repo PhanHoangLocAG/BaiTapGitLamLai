f14b9c2f556afb672a9dd6830f2e69be63ba10d4
Duyle2712 <duyspecter@gmail.com>
2020-11-14 00:29:24 +0700

    TinhTich

--- Source.cpp
+++ Source.cpp
@@ -3,12 +3,20 @@
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
-int tich(int a, int b);
+int tich(int a, int b) {
+	return a*b;
+}
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout << "====================================";
+	int p , q;
+	cout << "\nNhap p: ";
+	cin >> p;
+	cout << "\nNhap q: ";
+	cin >> q;
+	cout << "\nTich cua p*q = " << tich(p,q);
 	system("pause");
 	return 0;
 } 
