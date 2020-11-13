552d2666255a0f94954dfb8cb33443d9cba6db37
Chan Thien <huachanthien@gmail.com>
2020-11-13 23:38:55 +0700

    TinhHieu

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,21 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
+	int a,b;
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-
+	cout<<"\nNhap a:";
+	cin>>a;
+	cout<<"\nNhap b:";
+	cin>>b;
+	cout<<"Hieu la: "<<hieu(a,b);
 	system("pause");
 	return 0;
 } 
